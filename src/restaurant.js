c7d06f268288078c20b90f65084153feec592121
Felipe Duarte <feduarte.dev@gmail.com>
2023-06-02 15:04:46 -0300

    decimo requisito incompleto

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,6 +1,59 @@
 /* eslint-disable max-len */
 // Siga as orientações do README!
 
-const createMenu = () => {};
+// let objeto = { food: {}, drinks: {} };
 
+// const validaMenu = (item) => {
+//   if (
+//     Object.keys(restaurante.fetchMenu().food).includes(item) ||
+//     Object.keys(restaurante.fetchMenu().drinks).includes(item)
+//   ) {
+//     return 'Item indisponível';
+//   }
+//   consumption.push(item);
+// };
+
+const createMenu = (objeto) => {
+  const restaurante = {
+    fetchMenu: () => objeto,
+    consumption: [],
+    order: (item) => {
+      if (
+        !Object.keys(restaurante.fetchMenu().food).includes(item) &&
+        !Object.keys(restaurante.fetchMenu().drinks).includes(item)
+      ) {
+        return 'Item indisponível';
+      }
+      restaurante.consumption.push(item);
+    },
+  //   pay: () => {
+  //     let count = 0;
+  //     let comanda = restaurante.consumption;
+  //     let menuComida = Object.keys(restaurante.fetchMenu().food);
+
+  //     for (let index = 0; index < comanda.length; index += 1) {
+  //       for (let index1 = 0; index1 < menuComida.length; index1 += 1) {
+  //         if (comanda[index] === menuComida[index1]) {
+  //           count += Object.values(restaurante.fetchMenu().food)[index1];
+  //         }
+  //       }
+  //     }
+  //     return count;
+  //   },
+  };
+
+  return restaurante;
+};
+const objetoRetornadoCreateMenu = createMenu({
+  food: { coxinha: 3.9, sanduiche: 9.9 },
+  drinks: { agua: 3.9, cerveja: 6.9 },
+});
+
+objetoRetornadoCreateMenu.order('coxinha');
+objetoRetornadoCreateMenu.order('sanduiche');
+objetoRetornadoCreateMenu.order('coxinha');
+console.log(objetoRetornadoCreateMenu.consumption);
+
+// console.log(objetoRetornadoCreateMenu.pay());
 module.exports = createMenu;
+// console.log(Object.keys(restaurante.fetchMenu().food)[index]);
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -2,7 +2,24 @@ const createMenu = require('../src/restaurant');
  
 describe('10 - Implemente a função `createMenu`, bem como seus casos de teste', () => {
   it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
-    // Escreva todos os testes aqui.
+    // Requisito 1
+    const inventorio = createMenu({ food: {}, drinks: {} });
+    const objeto = { food: {}, drinks: {} };
+    const objetoRetornadoCreateMenu = createMenu(
+      {food: {coxinha: 3.90, sanduiche: 9.90},
+      drinks: {agua: 3.90, cerveja: 6.90}});
+    expect(createMenu()).toHaveProperty('fetchMenu');
+    expect(typeof inventorio.fetchMenu).toBe('function');
+    // Requisito 2
+    expect(createMenu({ food: {}, drinks: {} }).fetchMenu()).toHaveProperty('food');
+    expect(createMenu({ food: {}, drinks: {} }).fetchMenu()).toHaveProperty('drinks');
+    // Requisito 3
+    expect(objeto).toEqual(createMenu({ food: {}, drinks: {} }).fetchMenu());
+    // Requisito 5
+    expect(createMenu({ food: {}, drinks: {} })).toHaveProperty('consumption', []);
+    // Requisito 9/10
+    // expect(objetoRetornadoCreateMenu.order('agua')).toHaveProperty('consumption', 'agua');
+    // expect(objetoRetornadoCreateMenu.order('sanduiche')).toHaveProperty('consumption', []);
+    // expect(objetoRetornadoCreateMenu.order('agua')).toHaveProperty('consumption', []);
   });
 });
